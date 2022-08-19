b7f1d25e5dc42b4e1ae341479c0b347ea0decea1
Matt Wilson <mattwwilson34@gmail.com>
2022-08-18 21:19:21 -0400

    Add: imported fetchWeatherData func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+/* eslint-disable */
+
+import fetchWeatherData from './modules/fetch-weather-data.js';
+
+const getWeatherData = async () => {
+  const weatherData = await fetchWeatherData();
+  // console.log(JSON.stringify(weatherData, null, 4));
+};
+
+getWeatherData();
+
 // Testing for CI testing
 const add = (x, y) => x + y;
 
